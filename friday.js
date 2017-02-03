4205b44c49010d879a98342fa735f35e6fb9f013
Mousecop <joaskew13@gmail.com>
2017-02-03 16:00:40 -0500

    Added css file, made index.html with contianer, and solved shopping list challenge!

--- friday.js
+++ friday.js
@@ -1,86 +1,63 @@
 var appState = {
-    items: []
+    items: [
+      { entry: 'apples', status: true, currentlyEditing: false },
+      { entry: 'oranges', status: false }
+    ]
 };
-
-
-
-
-
 
 var addItem = function(appState, item, number) {
-    appState.items.push({ entry: item, status: false , value: number});
+    appState.items.push({ entry: item, status: false});
 };
 
+var deleteItem = function(appState, position) {
+    appState.items.splice(position, 1);
+};
 
+var checkItem = function(appState, position) {
+  appState.items[position].status = !appState.items[position].status;
+}
 
 //the render function takes in "the appState object" and the DOM 'element ' where it will be placed
 
 var renderList = function(state, element) {
-    var itemsHTML = state.items.map(function(indexItem) {
+    var itemsHTML = state.items.map(function(indexItem, i) {
+      var toggleHtml = "";
+      if (indexItem.status) {toggleHtml = "shopping-item__checked"};
         return '<li>' +
-            '<span class="shopping-item js-shopping-item"> ' + indexItem.entry + ' </span>' +
+            '<span class="shopping-item js-shopping-item '+ toggleHtml + '"> ' + indexItem.entry + ' </span>' +
             '<div class="shopping-item-controls">' +
-            '<button class="shopping-item-toggle">' +
+            '<button id="' + i + '" class="shopping-item-toggle css-toggle-button">' +
             '<span class="button-label">check</span>' +
             '</button>' +
-            '<button class="shopping-item-delete">' +
+            '<button id="' + i + '" class="shopping-item-delete">' +
             '<span class="button-label">delete</span>' +
             '</button>' +
             '</div>' +
             '</li>';
     });
     element.html(itemsHTML);
-    
+
 };
 
 $(function() {
+    renderList(appState, $('.shopping-list'));
+
     $('.shopping-list-add').submit(function(event) {
         event.preventDefault();
         addItem(appState, $('.shopping-list-add-input').val());
+        reset();
         renderList(appState, $('.shopping-list'));
-        // deleteItem(appState);
     });
-
-
-    $('.shopping-item-delete').click(function(event) {
-        var deleteClick = event.target;
-        if (deleteClick.className == "shopping-item-delete") {
-            deleteItem(appState, parseInt(deleteClick.parentNode.id));
-        }
-
+    $('ul').on('click', '.shopping-item-delete', function(event) {
+        deleteItem(appState, parseInt(event.currentTarget.id));
+        renderList(appState, $('.shopping-list'));
     });
-
+    $('ul').on('click', '.shopping-item-toggle', function(event) {
+        checkItem(appState, parseInt(event.currentTarget.id));
+        renderList(appState, $('.shopping-list'));
+    });
 });
-
-
-
-
-function deleteItem(appState, position) {
-    appState.items.splice(position, 1);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// to clear input field
+function reset() {
+  $('.shopping-list-add-input').val("");
+};
